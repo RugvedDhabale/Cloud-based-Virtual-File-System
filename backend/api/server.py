2b7d49df7c5227424e88ad835bdded88e1845274
RugvedDhabale <dhabalerugved7@gmail.com>
2026-03-14 21:00:21 +0530

    fix: CORS and Error Handeling

--- backend/api/server.py
+++ backend/api/server.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://cloud-based-virtual-file-system.vercel.app",
-        "http://localhost:5173",  # for local dev
+        "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
